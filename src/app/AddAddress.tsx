eed65c6ae96d5869190141874efbe69bbd1db6e7
blackdigitalgroup <andreellisjunior@gmail.com>
2023-04-19 19:13:46 -0500

    conditionally rendered the submit button as a means of confirmation address confirmation

--- src/app/AddAddress.tsx
+++ src/app/AddAddress.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddAddress = () => {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    const address = input;
+
+    console.log(address);
+  };
+
   return (
     <div className="add-address-wrapper">
       <h1>Add A New Address</h1>
-      <div className="input-wrapper">
-        <input type="text" value="" placeholder="Search/add an address" />
-        <button type="submit">ADD</button>
-      </div>
+      <form onSubmit={handleSubmit} className="input-wrapper">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Search/add an address"
+        />
+        {/* Conditionally rendered the submit button once something is typed into the input field */}
+        {input && <button type="submit">SAVE?</button>}
+      </form>
     </div>
   );
 };
